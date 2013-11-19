23f3277e4681b3ff2b19e41340dd6e5d2d3383fb
jacketattack <trvrmay@yahoo.com>
2013-11-18 19:33:20 -0800

    created abstract class for different Union-Find types to extend

---
+++ DynamicConnectivity.java
@@ -0,0 +1,33 @@
+
+public abstract class DynamicConnectivity {
+	private int[] roots;
+	
+	/**
+	 * This method sets the root of i, and all other 
+	 * roots that have a root equal to that of i, equal
+	 * to the root of j.
+	 *  
+	 * @param i first id to union together
+	 * @param j second one, keeps its root
+	 */
+	abstract void union(int i, int j);
+	
+	/**
+	 * This returns the root or parent node in the same set 
+	 * as the inputted node.
+	 * 
+	 * @param p The node to find its parent/root node
+	 * @return The head of the set that p is in
+	 */
+	abstract int find(int p);
+	
+	/**
+	 * Checks to see if the two nodes i and j are in the same
+	 * set.
+	 * 
+	 * @param i the first node 
+	 * @param j the second node 
+	 * @return true if have same root node, false otherwise
+	 */
+	abstract boolean connected(int i, int j);
+}
